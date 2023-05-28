e78498a95f6646d821e26c3d36d21a6a29d32917
saci zakaria <sacideveloppeur@gmail.com>
2023-05-28 22:41:27 +0200

    centerTitle: true,

--- lib/brandsScreens/upload_brands_screen.dart
+++ lib/brandsScreens/upload_brands_screen.dart
@@ -17,37 +17,37 @@ class _UploadBrandsScreenStateState extends State<UploadBrandsScreen> {
 
   Scaffold defaultScreen() {
     return Scaffold(
-    appBar: AppBar(
-      flexibleSpace: Container( decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.deepPurpleAccent.shade200,
-            Colors.amber.shade900,
-          ],
-          begin: const FractionalOffset(0.0, 0.0),
-          end: const FractionalOffset(1.0, 0.0),
-          stops: const [0.0, 1.0],
-          tileMode: TileMode.clamp,
-        ),
-      ),),
-      title: const Text(
-          "Add New Brand"
+      appBar: AppBar(
+        flexibleSpace: Container(
+            decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.deepPurpleAccent.shade200,
+              Colors.amber.shade900,
+            ],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: const [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
+        )),
+        title: const Text("Add New Brand"),
+        centerTitle: true,
       ),
-    ),
-    body: Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.deepPurpleAccent.shade200,
-            Colors.amber.shade900,
-          ],
-          begin: const FractionalOffset(0.0, 0.0),
-          end: const FractionalOffset(1.0, 0.0),
-          stops: const [0.0, 1.0],
-          tileMode: TileMode.clamp,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.deepPurpleAccent.shade200,
+              Colors.amber.shade900,
+            ],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: const [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
         ),
       ),
-    ),
-  );
+    );
   }
 }
