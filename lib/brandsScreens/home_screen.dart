84537f3ce5bb264a6489a54246c80c6e6dcdd97e
saci zakaria <sacideveloppeur@gmail.com>
2023-05-28 20:37:42 +0200

    IconButton Icons.abc_rounded

--- lib/brandsScreens/home_screen.dart
+++ lib/brandsScreens/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import '../widgets/my_drawer.dart';
@@ -12,22 +11,30 @@ class HomeScreen extends StatelessWidget {
       drawer: const MyDrawer(),
       appBar: AppBar(
         flexibleSpace: Container(
-          decoration:  BoxDecoration(
-              gradient: LinearGradient(colors: [
-                Colors.amber.shade300,
-                Colors.amber.shade800,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Colors.amber.shade300,
+              Colors.amber.shade800,
             ],
-              begin: FractionalOffset(0.0, 0.0),
-              end: FractionalOffset(1.0, 0.0),
-              stops: [0.0, 1.0],
-              tileMode: TileMode.clamp,)
-          ),
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          )),
         ),
         title: const Text(
           "iShop",
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.abc_rounded,
+              ))
+        ],
       ),
     );
   }
