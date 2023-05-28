5712f9613b5face999a264e68bd49fa707ffbb89
saci zakaria <sacideveloppeur@gmail.com>
2023-05-28 20:32:33 +0200

    class _UploadBrandsScreenState

---
+++ lib/brandsScreens/upload_brands_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _UploadBrandsScreenState extends StatefulWidget {
+  const _UploadBrandsScreenState({Key? key}) : super(key: key);
+
+  @override
+  State<_UploadBrandsScreenState> createState() => _UploadBrandsScreenStateState();
+}
+
+class _UploadBrandsScreenStateState extends State<_UploadBrandsScreenState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
