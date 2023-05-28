6104989708daae05eff875cbf1c2f55fc1bad936
saci zakaria <sacideveloppeur@gmail.com>
2023-05-28 22:35:11 +0200

    Scaffold defaultScreen() Extracted Methode

--- lib/brandsScreens/upload_brands_screen.dart
+++ lib/brandsScreens/upload_brands_screen.dart
@@ -12,22 +12,26 @@ class UploadBrandsScreen extends StatefulWidget {
 class _UploadBrandsScreenStateState extends State<UploadBrandsScreen> {
   @override
   Widget build(BuildContext context) {
+    return defaultScreen();
+  }
+
+  Scaffold defaultScreen() {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.deepPurpleAccent.shade200,
-              Colors.amber.shade900,
-            ],
-            begin: const FractionalOffset(0.0, 0.0),
-            end: const FractionalOffset(1.0, 0.0),
-            stops: const [0.0, 1.0],
-            tileMode: TileMode.clamp,
-          ),
+    appBar: AppBar(),
+    body: Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.deepPurpleAccent.shade200,
+            Colors.amber.shade900,
+          ],
+          begin: const FractionalOffset(0.0, 0.0),
+          end: const FractionalOffset(1.0, 0.0),
+          stops: const [0.0, 1.0],
+          tileMode: TileMode.clamp,
         ),
       ),
-    );
+    ),
+  );
   }
 }
