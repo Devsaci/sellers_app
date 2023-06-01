be1c837c25463cd50f7991832fdd6fbf99fc93c2
saci zakaria <sacideveloppeur@gmail.com>
2023-06-01 18:31:14 +0200

    child: const LinearProgressIndicator(),

--- lib/widgets/progress_bar.dart
+++ lib/widgets/progress_bar.dart
@@ -1,6 +1,8 @@
-
-
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-linearProgressBar(){return Container();}
+linearProgressBar() {
+  return Container(
+    child: const LinearProgressIndicator(),
+  );
+}
