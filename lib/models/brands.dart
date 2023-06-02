2275ea2519b82ae6be51e93407356df6c860cbae
saci zakaria <sacideveloppeur@gmail.com>
2023-06-02 20:05:23 +0200

    Brands({});

--- lib/models/brands.dart
+++ lib/models/brands.dart
@@ -8,4 +8,14 @@ class Brands {
   String? sellerUID;
   String? status;
   String? thumbnailUrl;
+
+  Brands({
+    this.brandID,
+    this.brandInfo,
+    this.brandTitle,
+    this.publishedDate,
+    this.sellerUID,
+    this.status,
+    this.thumbnailUrl,
+  });
 }
