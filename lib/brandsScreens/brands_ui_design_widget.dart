4e23ab6c683ab6e5c46f6eec4ffd994453b165eb
saci zakaria <sacideveloppeur@gmail.com>
2023-06-02 20:21:34 +0200

    class BrandsUiDesignWidget extends StatefulWidget

---
+++ lib/brandsScreens/brands_ui_design_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BrandsUiDesignWidget extends StatefulWidget {
+  const BrandsUiDesignWidget({Key? key}) : super(key: key);
+
+  @override
+  State<BrandsUiDesignWidget> createState() => _BrandsUiDesignWidgetState();
+}
+
+class _BrandsUiDesignWidgetState extends State<BrandsUiDesignWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
