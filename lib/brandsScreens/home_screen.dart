3eb73924e72448c8b9ee950a63cdaddf4521633c
saci zakaria <sacideveloppeur@gmail.com>
2023-06-05 20:41:07 +0200

    StreamBuilder stream:

--- lib/brandsScreens/home_screen.dart
+++ lib/brandsScreens/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sellers_app/brandsScreens/upload_brands_screen.dart';
+import 'package:sellers_app/global/global.dart';
 import 'package:sellers_app/widgets/TextDelegateHeaderWidget.dart';
 
 import '../widgets/my_drawer.dart';
@@ -15,15 +18,15 @@ class HomeScreen extends StatelessWidget {
         flexibleSpace: Container(
           decoration: BoxDecoration(
               gradient: LinearGradient(
-                colors: [
-                  Colors.amber.shade300,
-                  Colors.amber.shade800,
-                ],
-                begin: FractionalOffset(0.0, 0.0),
-                end: FractionalOffset(1.0, 0.0),
-                stops: [0.0, 1.0],
-                tileMode: TileMode.clamp,
-              )),
+            colors: [
+              Colors.amber.shade300,
+              Colors.amber.shade800,
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          )),
         ),
         title: const Text(
           "iShop",
@@ -33,8 +36,11 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (c) => const UploadBrandsScreen(),));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (c) => const UploadBrandsScreen(),
+                    ));
               },
               icon: const Icon(
                 Icons.abc_rounded,
@@ -45,11 +51,24 @@ class HomeScreen extends StatelessWidget {
         slivers: [
           SliverPersistentHeader(
             pinned: true,
-            delegate:TextDelegateHeaderWidget(title: ("My Brand")),
+            delegate: TextDelegateHeaderWidget(
+              title: ("My Brand"),
+            ),
           ),
+          //1. write query
+          //2  model
+          //3. ui design widget
+          StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("sellers")
+                .doc(sharedPreferences!.getString("uid"))
+                .collection("brands")
+                .orderBy("publishedDate", descending: true)
+                .snapshots(),
+            builder: null, // ERROE
+          )
         ],
       ),
-
     );
   }
 }
