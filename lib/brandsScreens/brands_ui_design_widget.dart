5ecd022c2b5fd272162061d3f447618d942653c5
saci zakaria <sacideveloppeur@gmail.com>
2023-06-06 21:34:37 +0200

    return GestureDetector child: Card

--- lib/brandsScreens/brands_ui_design_widget.dart
+++ lib/brandsScreens/brands_ui_design_widget.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import '../models/brands.dart';
 
 class BrandsUiDesignWidget extends StatefulWidget {
-  Brands? model;
-  BuildContext? context;
+  final Brands? model;
+  final BuildContext? context;
 
-  BrandsUiDesignWidget({this.model, this.context,});
+  const BrandsUiDesignWidget({super.key,
+    this.model,
+    this.context,
+  });
 
   @override
   State<BrandsUiDesignWidget> createState() => _BrandsUiDesignWidgetState();
@@ -15,49 +18,52 @@ class BrandsUiDesignWidget extends StatefulWidget {
 class _BrandsUiDesignWidgetState extends State<BrandsUiDesignWidget> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 10,
-      shadowColor: Colors.black,
-      child: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: SizedBox(
-          height: 270,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              Image.network(
-                widget.model!.thumbnailUrl.toString(),
-                height: 220,
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(height: 1),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    widget.model!.brandTitle.toString(),
-                    style: const TextStyle(
-                      color: Colors.deepPurple,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      letterSpacing: 3,
-                    ),
-                  ),
-                  //Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.delete_sweep,
-                        color: Colors.pinkAccent,
-                        size: 50,
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        elevation: 10,
+        shadowColor: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: SizedBox(
+            height: 270,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                Image.network(
+                  widget.model!.thumbnailUrl.toString(),
+                  height: 220,
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 1),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      widget.model!.brandTitle.toString(),
+                      style: const TextStyle(
+                        color: Colors.deepPurple,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        letterSpacing: 3,
                       ),
                     ),
-                  )
-                ],
-              )
-            ],
+                    //Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.delete_sweep,
+                          color: Colors.pinkAccent,
+                          size: 50,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
