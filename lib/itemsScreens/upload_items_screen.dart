62f76c0ab75dab3b86d86335e40f0f326693eb4e
saci zakaria <sacideveloppeur@gmail.com>
2023-06-07 17:55:41 +0200

    class UploadItemsScreen extends StatefulWidget

---
+++ lib/itemsScreens/upload_items_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class UploadItemsScreen extends StatefulWidget {
+  const UploadItemsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<UploadItemsScreen> createState() => _UploadItemsScreenState();
+}
+
+class _UploadItemsScreenState extends State<UploadItemsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
